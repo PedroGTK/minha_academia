08a4307fc571b47c493a52785ce3cbee7ebc1b55
Pedro Edilson <pedroedilson27@gmail.com>
2024-07-04 17:35:31 -0300

    minha_academia

---
+++ save_aula/utils.js
@@ -0,0 +1,18 @@
+module.exports = {
+ age: function (timestamp){
+        const today = new Date() // controla a data de hoje
+        const birthday = new Date (timestamp) // aniversario em segundos
+
+        let age = today.getFullYear() - birthday.getFullYear()
+        let month = today.getMonth() - birthday.getMonth()
+
+        if(month < 0 || month == 0 && today.getDate() < birthday.getDate()){
+        age = age -1
+        }
+
+        return age
+
+    }
+}
+
+
